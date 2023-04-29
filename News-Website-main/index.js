ca5f3b9868ace8275cf7d60cc30b47315cfcd594
aman7cr7 <107622127+aman7cr7@users.noreply.github.com>
2023-04-29 16:43:02 +0530

    Add existing projectt

--- News-Website-main/index.js
+++ News-Website-main/index.js
@@ -14,7 +14,7 @@ const newsdetails = document.getElementById("newsdetails");
 var newsDataArr = [];
 
 // apis 
-const API_KEY = "c06c010dd62a4f62b01612dfeb03778b";
+const API_KEY = "0ab8409d64874cb782b723a742cf1921";
 const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
 const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
 const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
